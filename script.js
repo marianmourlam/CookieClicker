d43b1aa6b960198f0b6ee90b356d6e3962b26a09
Marian Mourlam <marian.mourlam@gmail.com>
2026-02-24 14:24:20 +0100

    click counter implementation (#4)
    * add a cookie image + adds style to the cookie and image
    
    * create main + body height added + div added to center our image + viewport meta tag
    
    * improve architecture + refacto css
    
    * Add h2 counter and remove top margin
    
    * add counter when user clicked on cookie
    
    * add defer to script js + rename h2 to score + rename compteur to counter delete flex h2 + add id cookieButton
    
    * remove css body

---
+++ script.js
@@ -0,0 +1,9 @@
+let counter = 0;
+
+const cookie = document.getElementById("cookieButton");
+const score = document.getElementById("score");
+
+cookie.addEventListener("click", () => {
+    counter++;
+    score.textContent = counter;
+});
