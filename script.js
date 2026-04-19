fbf72b8984cba3017372a5793dad471d50033fe5
Marian Mourlam <marian.mourlam@gmail.com>
2026-04-19 12:08:38 +0200

    feat(script.js): enhance showUpgrades function to display upgrade description and base cost for better user information

--- script.js
+++ script.js
@@ -27,11 +27,18 @@ function showUpgrades(upgrades) {
     const shop = document.getElementById("shop");
 
     for (const upgrade of upgrades) {
+        const description = document.createElement("p");
+        const baseCost = document.createElement("p")
         const parent = document.createElement("div");
         const child = document.createElement("h3");
 
         child.innerText = upgrade.name;
+        description.innerText = upgrade.description;
+        baseCost.innerText = upgrade.baseCost;
+
         parent.appendChild(child);
+        parent.appendChild(description);
+        parent.appendChild(baseCost);
         shop.appendChild(parent);
     }
 }
