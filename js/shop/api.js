20e955edf907403063fff818ff0ed2303107f137
Marian Mourlam <marian.mourlam@gmail.com>
2026-09-10 09:30:01 +0200

    feat: load upgrades from PHP endpoint

--- js/shop/api.js
+++ js/shop/api.js
@@ -1,5 +1,5 @@
 export async function loadUpgrades() {
-  const response = await fetch("./shop.json");
+  const response = await fetch("./php/index.php");
   if (!response.ok) {
     throw new Error("Failed to load upgrade");
   }
